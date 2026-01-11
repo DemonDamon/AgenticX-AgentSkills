f27e13cace70fa3f9aed9c74ca0cd8940c23432d
DemonDamon <bingzhenli@hotmail.com>
2026-01-11 19:31:58 +0800

    feat(frontend): integrate i18n provider into app
    - Wrap application with I18nProvider
    - Enable internationalization across all components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { I18nProvider } from './i18n'
 import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
 import SkillsPage from './pages/SkillsPage'
@@ -6,15 +7,17 @@ import SkillDetailPage from './pages/SkillDetailPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/skills" element={<SkillsPage />} />
-          <Route path="/skills/:id" element={<SkillDetailPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <I18nProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/skills" element={<SkillsPage />} />
+            <Route path="/skills/:id" element={<SkillDetailPage />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </I18nProvider>
   )
 }
 
